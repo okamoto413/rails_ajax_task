97e5f02afcaec76413a7bca7999ba6db70726b3e
kujira12 <ha9botan@gmail.com>
2024-04-19 16:34:52 +0900

    Ajaxによるお気に入り機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,22 @@
 class FavoritesController < ApplicationController
-
+ #redirect_toを使用せず非同期でページをレンダリングすること
   def create
-    current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    @favorite = current_user.favorites.create(blog_id: params[:blog_id])
+    @blog = @favorite.blog
+    respond_to do |format|
+      format.html { redirecto_to :blogs }
+      format.js {render :index }
+    end 
   end
 
+ #redirect_toを使用せず非同期でページをレンダリングすること
   def destroy
-    current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    @favorite = current_user.favorites.find_by(id: params[:id])
+    @blog = @favorite.blog
+    @favorite.destroy if @favorite
+    respond_to do |format|
+      format.html { redirecto_to :blogs }
+      format.js  {render :index }
+    end  
   end
-
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+// Rails6でjQueryを利用するため追加
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+
 module.exports = environment
